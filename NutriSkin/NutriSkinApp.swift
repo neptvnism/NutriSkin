e691d2f6f2ef3a3804e629a21c31d254ac0afde8
neptvnism <ihab.n.124@gmail.com>
2024-07-03 13:22:28 +0300

    Initial Commit

---
+++ NutriSkin/NutriSkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriSkinApp.swift
+//  NutriSkin
+//
+//  Created by Nadeen Ihab  on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NutriSkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
